3201b89b7f2aa04a504d34d4e91a9c1db46f4c7a
Marco Pla <marco.pla@gmail.com>
2023-11-30 21:48:40 -0300

    POST refactoring

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -2,18 +2,28 @@ const express = require('express');
 
 const router = express.Router();
 
+const Checklist = require('../models/checklist');
+
 router.get('/', (req, res) => {
   console.log('Olá');
   res.send();
 });
 
-router.post('/', (req, res) => {
-  console.log(req.body);
+router.post('/', async (req, res) => {
+  let { name } = req.body;
+  try{
+    let checklist = await Checklist.create({name}) 
+    
+  }catch(error){
+
+  }
+  console.log({ name });
   res.status(200).json(req.body);
 });
 
 router.get('/:id', (req, res) => {
   console.log(req.params);
+
   res.send(`ID: ${req.params.id}`);
 });
 
