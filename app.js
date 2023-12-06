3491b8826a1d1ce01856e33b4e9af6402150f300
Marco Pla <marco.pla@gmail.com>
2023-12-05 22:33:51 -0300

    partials added

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ app.set('view engine', 'ejs');
 
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/', rootRouter);
 app.use('/checklists', checkListRouter);
 
